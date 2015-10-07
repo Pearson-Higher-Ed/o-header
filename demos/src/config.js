c6326b7da4cfaf27515500c211d6b50289a3bf50
Adam Armour <adam.armour@gmail.com>
2015-10-07 09:20:08 -0600

    feat: es2015
    - Remove unused js: Header and tests
    - Update demo js to pass eslint
    - Remove unused dependencies

--- demos/src/config.js
+++ demos/src/config.js
@@ -1,37 +1,35 @@
-'use strict';
-
-var fs = require('fs');
+const fs = require('fs');
 
 module.exports = {
-	"options": {
-		"js": "demos/src/demo.js",
-		"sass": "demos/src/scss/demo.scss",
-		"template": "main.mustache",
-		"data": {
-			"content": fs.readFileSync('demos/src/html/content.html')
+	options: {
+		js: 'demos/src/demo.js',
+		sass: 'demos/src/scss/demo.scss',
+		template: 'main.mustache',
+		data: {
+			content: fs.readFileSync('demos/src/html/content.html')
 		},
-		"dependencies": [
-			"o-dropdown-menu@0.5.0",
-			"o-fonts@0.2.0"
+		dependencies: [
+			'o-dropdown-menu@0.5.0',
+			'o-fonts@0.2.0'
 		]
 	},
-	"demos": [
+	demos: [
 		{
-			"name": "basic",
-			"data": {
-				"o-header": {
-					"container": {
-						"extra-classes": "demo-container"
+			name: 'basic',
+			data: {
+				'o-header': {
+					container: {
+						'extra-classes': 'demo-container'
 					},
-					"sections": fs.readFileSync('demos/src/html/basic-sections.html')
+					sections: fs.readFileSync('demos/src/html/basic-sections.html')
 				}
 			}
 		},
 		{
-			"name": "nav",
-			"data": {
-				"o-header": {
-					"sections": fs.readFileSync('demos/src/html/nav-sections.html')
+			name: 'nav',
+			data: {
+				'o-header': {
+					sections: fs.readFileSync('demos/src/html/nav-sections.html')
 				}
 			}
 		}
